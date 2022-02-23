639a77b93710de21f6e9d35b3e0ab2be5888d2cb
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-02-23 14:04:54 +0100

    up

--- aula_teste.sql
+++ aula_teste.sql
@@ -1,8 +1,17 @@
+# criando o banco de dados
 CREATE DATABASE AULA_TESTE;
 
+# criando a primeira tabela (veiculos)
 CREATE TABLE VEICULOS
 (PLACA VARCHAR(7) NOT NULL,
 MODELO VARCHAR(20) NOT NULL,
 ANO INTEGER);
 
-SELECT * FROM aula_teste.veiculos;
+# selecionando/carregando a tabela veiculos (vazia) no mysql
+SELECT * FROM veiculos;
+
+# criando a tabela cores
+CREATE TABLE COR (CODIGO INTEGER NOT NULL, DESC_COR VARCHAR(10));
+
+# selecionando/carregando a tabela cor (vazia) no mysql
+SELECT * FROM cor;
