29e3cc5e36c8ebe893c42995b1101bd10b09a653
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-02-23 14:26:08 +0100

    up

--- aula_teste.sql
+++ aula_teste.sql
@@ -1,3 +1,5 @@
+# 1) definição de dados
+
 # criando o banco de dados
 CREATE DATABASE AULA_TESTE;
 
@@ -15,3 +17,22 @@ CREATE TABLE COR (CODIGO INTEGER NOT NULL, DESC_COR VARCHAR(10));
 
 # selecionando/carregando a tabela cor (vazia) no mysql
 SELECT * FROM cor;
+
+
+# 2) manipulação de dados
+
+# inserindo as cores na tabela 
+INSERT INTO COR (CODIGO, DESC_COR) VALUES (1, 'AZUL');
+INSERT INTO COR (CODIGO, DESC_COR) VALUES (2, 'ROSA');
+INSERT INTO COR (CODIGO, DESC_COR) VALUES (3, 'VERDE');
+INSERT INTO COR (CODIGO, DESC_COR) VALUES (4, 'AMARELO');
+
+# deletar uma linha na tabela
+DELETE FROM cor
+WHERE CODIGO = 1;
+
+# alteração de uma informação na tabela
+UPDATE COR
+SET CESC_COR = 'BRANCO'
+WHERE CODIGO = 3;
+
